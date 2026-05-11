f82d43dc0fb76c6609c1c36fdb281b3bb8d3dfb7
HajarBoulmane <hajarboulmane2003@gmail.com>
2026-05-11 08:52:15 +0100

    first commit

---
+++ src/main/java/net/hajar/boulmanehajarexamjee/BoulmaneHajarExamJeeApplication.java
@@ -0,0 +1,13 @@
+package net.hajar.boulmanehajarexamjee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoulmaneHajarExamJeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoulmaneHajarExamJeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/hajar/boulmanehajarexamjee/BoulmaneHajarExamJeeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.hajar.boulmanehajarexamjee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoulmaneHajarExamJeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
